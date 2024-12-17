fec026a2a9a1bb8cf1ed8cd60f63eaaf878a2de3
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-12-17 13:59:19 +0000

    first commit on this branch

--- app/api/(auth)/users/route.ts
+++ app/api/(auth)/users/route.ts
@@ -1,8 +1,22 @@
 import {NextResponse} from "next/server";
+// import connect from "@/lib/db"
+import User from "@/lib/models/user";
+import connect from "@/lib/db";
 
-export const GET = () => {
-    return new NextResponse (
-        "This is the begining of learning api with NextJs"
-    )
-    
-}
+
+export const GET = async () => {
+    try {
+        await connect();
+        const users = await User.find();
+        return new NextResponse(JSON.stringify(users), {status: 200})
+    } catch (error: any) {
+        return new NextResponse ("Error in Fetching Users" + error.message, {status: 500})
+    }
+    return new NextResponse ("This is the beginning of learning api with NextJs") 
+} 
+
+
+
+
+
+
