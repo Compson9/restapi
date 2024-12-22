a9d4596fd8e59329a0098e7cbe112a1431db48c7
Elvis Compson <105922474+Compson9@users.noreply.github.com>
2024-12-22 09:38:08 +0000

    my first commit this morning on sunday lets go

--- app/api/(dashboard)/blogs/[blog]/route.ts
+++ app/api/(dashboard)/blogs/[blog]/route.ts
@@ -81,3 +81,103 @@ export const GET = async (request: Request, context: { params: { blog: string } 
 
     }
 }
+
+export const PATCH = async (request: Request, context: { params: any }) => {
+    const blogId = context.params.blog;
+
+
+    try {
+        const body = await request.json();
+        const { title, description } = body;
+
+        const { searchParams } = new URL(request.url);
+        const userId = searchParams.get("userId");
+
+        if (!userId || !Types.ObjectId.isValid(userId)) {
+            return new NextResponse(
+                JSON.stringify({ message: "Invalid or missing userId" }),
+                { status: 400 }
+            );
+        }
+
+        if (!blogId || !Types.ObjectId.isValid(blogId)) {
+            return new NextResponse(JSON.stringify({ message: "Invalid or missing BlogId" }),
+                { status: 400 })
+        }
+
+        await connect();
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return new NextResponse(
+                JSON.stringify({ message: "User not found" }),
+                { status: 400 }
+            );
+        }
+
+        const blog = await Blog.findOne({
+            _id: blogId,
+            user: userId,
+        });
+
+
+        if (!blog) {
+            return new NextResponse(JSON.stringify({
+                message: "Blog not found"
+            }),
+                { status: 400 }
+            );
+        }
+
+        const updatedBlog = await Blog.findByIdAndUpdate(
+            blogId,
+            { title, description },
+            { new: true }
+        );
+
+        return new NextResponse(JSON.stringify({ message: "Blog Updated", blog: updatedBlog }),
+            { status: 200 }
+        )
+
+
+
+
+
+    } catch (error: unknown) {
+        const errorMessage = error instanceof Error ? error.message : "Unknown error";
+        return new NextResponse(
+            JSON.stringify({ message: "Error updating Blog: " + errorMessage }),
+            { status: 500 }
+        );
+    }
+}
+
+export const DELETE = async(request: Request, context: {params: any}) => {
+    const blogId = context.params.blog;
+    
+    try {
+        const { searchParams } = new URL(request.url);
+        const userId = searchParams.get("userId");
+
+        if (!userId || !Types.ObjectId.isValid(userId)) {
+            return new NextResponse(
+                JSON.stringify({ message: "Invalid or missing userId" }),
+                { status: 400 }
+            );
+        }
+
+        if (!blogId || !Types.ObjectId.isValid(blogId)) {
+            return new NextResponse(JSON.stringify({ message: "Invalid or missing BlogId" }),
+                { status: 400 })
+        }
+
+        await connect();
+        
+    } catch (error: unknown) {
+        const errorMessage = error instanceof Error ? error.message : "Unknown error";
+        return new NextResponse(
+            JSON.stringify({ message: "Error in Deleting blog: " + errorMessage }),
+            { status: 500 }
+        );
+    }
+}
